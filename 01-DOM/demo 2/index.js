d74637efcd4ed76449d08daab50094a3f8a85dc8
Matias Ruiz Silva <ruizsilvamatias@gmail.com>
2022-07-19 17:46:22 -0300

    first push

---
+++ 01-DOM/demo 2/index.js
@@ -0,0 +1,6 @@
+let divs = document.getElementsByTagName('div')
+for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', function (e){console.log('Click: '+ e.target.innerText)})
+
+    
+}
